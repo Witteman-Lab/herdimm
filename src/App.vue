5a57759f3474ad746ec3612dce64f705243c3a89
Donavan <donavan.aziaka.1@ulaval.ca>
2019-02-13 19:03:49 -0500

    move some function to App.vue in order to multiple Character with color implementation

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" :src="require('./assets/logo.png')">
-    <Character msg="Welcome to Your Vue.js App"/>
+    <div>
+      <Character style="cursor: pointer"  ref="character" :svgFile="require('./assets/F-1.svg')" :color="this.currentColor" />
+    </div>
+    <div>
+      <Character style="cursor: pointer"  ref="character" :svgFile="require('./assets/F-6.svg')" :color="this.currentColor" />
+    </div>
+    <div>
+      <Character style="cursor: pointer"  ref="character" :svgFile="require('./assets/F-1.svg')" :color="this.currentColor" />
+    </div>
+    <br/>
+    <div style="display:flex; justify-content: center;">
+      <slider-picker :value="this.currentColor" @input="this.updateValue"/>
+    </div>
+    <br/>
+    <br/>
+    <button v-on:click="this.resetFaceColor">Reset Color</button>
+    <button v-on:click="this.changeFaceColor">Change Face Color</button>
+    <button v-on:click="this.changeHairColor">Change Hair Color</button>
   </div>
 </template>
 
 <script>
-import Character from './components/Character.vue'
+  import Character from './components/Character.vue'
+  import { Slider } from 'vue-color';
 
-export default {
-  name: 'app',
-  components: {
-      Character
+  export default {
+    name: 'app',
+    components: {
+      Character,
+      'slider-picker': Slider,
+    },
+    data() {
+      return {
+        currentColor: '#FFFFFF',
+        tab:['test1', 'test2', 'test3'],
+        currentCharacter: "character"
+      };
+    },
+    methods: {
+      updateValue(color) {
+        this.currentColor = color.hex;
+      },
+      changeFaceColor() {
+        this.$refs[this.currentCharacter].changeFaceColor();
+      },
+      changeHairColor(){
+        this.$refs[this.currentCharacter].changeHairColor();
+      },
+      resetFaceColor(){
+        this.$refs[this.currentCharacter].resetFaceColor();
+      },
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
--- src/components/Character.vue
+++ src/components/Character.vue
@@ -4,25 +4,12 @@
         <div style="display: flex; justify-content: center">
             <div ref="characterImg" v-html="this.svg" style="width: 128px; height: 119px;"></div>
         </div>
-        <div style="display:flex; justify-content: center;">
-            <slider-picker :value="this.currentColor" @input="this.updateValue"/>
-        </div>
-        <br/>
-        <br/>
-        <button v-on:click="this.resetFaceColor">Reset Color</button>
-        <button v-on:click="this.changeFaceColor">Change Face Color</button>
-        <button v-on:click="this.changeHairColor">Change Hair Color</button>
     </div>
 </template>
 
 <script>
-    /* eslint-disable no-console */
-
-    import { Slider } from 'vue-color';
     export default {
-
         components: {
-            'slider-picker': Slider,
         },
         name: "Character",
         data() {
@@ -39,21 +26,18 @@
             };
         },
         props: {
-
+            color: String,
+            svgFile: String
         },
         methods: {
-            updateValue(color) {
-                console.log(color.hex);
-                this.currentColor = color.hex;
-            },
             changeFaceColor() {
-                this.svgColor.face = this.currentColor;
+                this.svgColor.face = this.color;
                 this.characterInstance.innerHTML =
                     this.characterInstance.innerHTML + `.cls-3{fill:${this.svgColor.face}}`;
             },
             changeHairColor(){
-                this.svgColor.hairFront = this.currentColor;
-                this.svgColor.hairBack = this.currentColor;
+                this.svgColor.hairFront = this.color;
+                this.svgColor.hairBack = this.color;
                 this.characterInstance.innerHTML = this.characterInstance.innerHTML +
                     `.cls-5{fill:${this.svgColor.hairFront}}.cls-6{fill:${this.svgColor.hairBack}}`;
             },
@@ -62,14 +46,12 @@
             }
         },
         created() {
-            this.svg = require('../assets/F-1.svg');
+            this.svg = this.svgFile;
         },
         mounted() {
-           this.characterInstance = this.$refs.characterImg.children[0].children[0].children[0];
-           this.defaultColor = this.characterInstance.innerHTML;
+            this.characterInstance = this.$refs.characterImg.children[0].children[0].children[0];
+            this.defaultColor = this.characterInstance.innerHTML;
         },
-        beforeUpdate() {
-        }
     }
 </script>
 
