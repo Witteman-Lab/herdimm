0668cab251c131bb4d9944a84becac9f8c85db22
Martin T.-B <martin_t_b@hotmail.com>
2019-03-29 16:32:20 -0400

    Update Shapes.vue
    Attempt to load svg character into a shape on canvas

--- src/components/Shapes.vue
+++ src/components/Shapes.vue
@@ -1,13 +1,16 @@
 <template>
     <v-stage ref="stage" :config="configStage">
-        <v-layer ref="group">
+        <!-- <v-layer ref="group"> -->
             <!-- <img src="http://www.w3.org/TR/SVG11/images/painting/fillrule-evenodd.svg" /> -->
             <!-- <object type="image/svg+xml" data="../assets/characters/adult01.svg" /> -->
-        </v-layer>
+        <!-- </v-layer> -->
 
         <v-layer ref="grid">
             <div>
-                <v-regular-polygon class="" ref="hexagon" :config="configHexagon" v-on:click="changeColor"></v-regular-polygon>
+                <v-regular-polygon name="shape1" class="" ref="hexagon" :config="configHexagon" v-on:click="changeColor"></v-regular-polygon>
+            </div>
+            <div>
+                <GroupCharacter ref="group"></GroupCharacter>
             </div>
         </v-layer>
 
@@ -15,8 +18,10 @@
 </template>
 
 <script>
-    //import Character from "./Character";
+    import GroupCharacter from "./GroupCharacter";
     import json from '../assets/characters.json';
+    import VueKonva from 'vue-konva';
+    import svgFile from '../assets/characters/adult01.svg';
 
     const width = window.innerWidth;
     const height = window.innerHeight;
@@ -24,7 +29,12 @@
     export default {
         name: "Shapes",
         components: {
-            //Character,
+            GroupCharacter
+        },
+        props: {
+            group: {
+                type: Array
+            }
         },
         data() {
             return {
@@ -40,6 +50,7 @@
                     sides: 6,
                     radius: 80,
                     stroke: "black",
+                    strokeWidth: 5,
                     // fill: "grey",
                     rotation: 90
                 }
@@ -54,13 +65,13 @@
             changeColor(evt) {
                 var r = evt.currentTarget.x() % 255;
                 var g = evt.currentTarget.y() % 255;
+                var b = evt.currentTarget.x() * evt.currentTarget.y() % 255;
 
                 evt.currentTarget.x(evt.currentTarget.x() + 5);
                 evt.currentTarget.y(evt.currentTarget.y() + 5);
 
-                //evt.currentTarget.fill(this.characterList[0].file);
-                // evt.currentTarget.fill("rgb(" + r + ", " + g + ", 100)");
-                //evt.currentTarget.strokeWidth(evt.currentTarget.strokeWidth() + 1);
+                //evt.currentTarget.fill(svgFile);
+                evt.currentTarget.fill("rgb(" + r + ", " + g + ", " + b + ", 100)");
                 evt.currentTarget.stroke("#FF0000");
 
                 // Required to apply the change
@@ -71,27 +82,29 @@
             this.characterList = json.characters;
         },
         mounted() {
+            // if (this.group) {
+            //     //console.log(this.group);
+            //     localStorage.setItem("group", JSON.stringify(this.group));
+            //     this.$refs.group.addGroup(this.group);
+            // } else {
+            //     if (localStorage.getItem("group"))
+            //         this.$refs.group.addGroup(JSON.parse(localStorage.getItem("group")));
+            // }
+
             let grid = this.$refs.grid;
             let hexagon = this.$refs.hexagon;
             let stage = this.$refs.stage;
-            let group = this.$refs.group;
-            var img = new Image();
+            // let group = this.$refs.group;
 
-            img.onload = function () {
-                console.log("loaded");
-                console.log("grid", grid);
-                console.log("grid.getStage()", grid.getStage());
-                console.log("hexagon", hexagon);
-                console.log("hexagon.getStage()", hexagon.getStage());
-                console.log("stage", stage);
-                console.log("stage.getStage()", stage.getStage());
+            //console.log(svgFile);
 
-                //grid.getStage().drawImage(img,0,0);
-                //hexagon.fill(img);
-                //grid.getStage().draw();
-            };
-            // img.src = "../assets/characters/" + this.characterList[0].file;
-            img.src = "http://www.w3.org/TR/SVG11/images/painting/fillrule-evenodd.svg";
+            // Konva.Image.fromURL("http://www.w3.org/TR/SVG11/images/painting/fillrule-evenodd.svg", function(image){
+            Konva.Image.fromURL(svgFile, function(image){
+               // image is Konva.Image instance
+               console.log("image", image);
+               grid.getStage().add(image);
+               grid.getStage().draw();
+            });
         }
     }
 </script>
