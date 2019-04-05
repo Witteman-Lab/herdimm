c8b1c68eecfeb5122b4894e9fa8257f8aef1060d
Martin T.-B <martin_t_b@hotmail.com>
2019-04-05 12:18:37 -0400

    Add audio components (draft)

---
+++ src/components/AudioPlayer.vue
@@ -0,0 +1,28 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+import audio from "../assets/audio.json";
+export default {
+    name: "AudioPlayer",
+    components: {},
+    data() {
+        return {}
+    },
+    props: {},
+    methods: {},
+    created() {
+        console.log("created");
+    },
+    mounted() {
+        console.log("mounted");
+        //console.log("audio", audio);
+        //var audio = new Audio(require('../../assets/testaudio.mp3'));
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
