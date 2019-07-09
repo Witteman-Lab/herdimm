0c0dcb7b10844633d43758f4d7cd8363e2d7f050
Martin T.-B <martin_t_b@hotmail.com>
2019-07-09 09:00:23 -0400

    More tests for IE11 support

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   presets: [
-    '@vue/app'
+    '@vue/app',
+    ["env", {
+      "targets": {
+        // The % refers to the global coverage of users from browserslist
+        "browsers": [ "> 1%", "last 2 versions" ]
+      }
+    }]
   ]
 }
