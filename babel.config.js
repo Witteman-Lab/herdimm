6f2dd38fd7d62814fb1b8e6d8bc425001b456795
Martin T.-B <martin_t_b@hotmail.com>
2019-07-09 09:03:37 -0400

    Previous file produced building error

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,5 @@
 module.exports = {
   presets: [
-    '@vue/app',
-    ["env", {
-      "targets": {
-        // The % refers to the global coverage of users from browserslist
-        "browsers": [ "> 1%", "last 2 versions" ]
-      }
-    }]
+    '@vue/app'
   ]
 }
