ef7d3e2d9559f4db2810b1830c68bf0521a665a0
Donavan <donavan.aziaka.1@ulaval.ca>
2019-08-18 16:08:51 -0400

    handle multiple character to save

--- api/exampleMongoDB.js
+++ api/exampleMongoDB.js
@@ -2,47 +2,50 @@ var express = require('express');
 var router = express.Router();
 
 var mongoose = require("mongoose");
-mongoose.connect('mongocharacterDb://localhost:27017/api_herdimm');
+mongoose.connect('mongodb://localhost:27017/api_herdimm', { useNewUrlParser: true });
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+
 var mongoOp = require("./models/mongo");
 
 const createCharacter = (request, response) => {
     console.log(request.body);
-    const characterList = request.body;
     var characterDb = new mongoOp();
     var res = {};
     // fetch email and password from REST request.
     // Add strict validation when you use this in Production.
-    const character = {
-        userid: 0,
-        name: characterList[0].colors.name,
-        type: characterList[0].characterType,
-        vulnerabilities: characterList[0].colors.options,
-        svgFile: characterList[0].file,
-        facecolor: characterList[0].colors.face,
-        haircolor: characterList[0].colors.hairFront,
-        glassesid: characterList[0].colors.glasses,
-        beardsid: characterList[0].colors.beards
-    };
 
-    characterDb.userId = character.userid;
-    characterDb.name = character.name;
-    characterDb.type = character.type;
-    characterDb.vulnerabilities = character.vulnerabilities;
-    characterDb.svgFile = character.svgFile;
-    characterDb.faceColor = character.facecolor;
-    characterDb.hairColor = character.haircolor;
-    characterDb.glassesId = character.glassesid;
-    characterDb.beardsId = character.beardsid;
+    const characterList = charactersList(request.body);
 
-    characterDb.save(function(err) {
+    characterDb.collection.insertMany(characterList, function (err, docs) {
         if (err) {
-            res = {"error" : true,"message" : "Error adding data"};
+                return console.error(err);
         } else {
-            res = {"error" : false,"message" : "Data added"};
+            console.log("Multiple documents inserted to Collection");
         }
         response.json(res);
     });
 };
+
+const charactersList = (characterList) => {
+    const characterDbList = [];
+    characterList.forEach((character) => {
+        var characterObject = {
+            userId: 0,
+            name: character.colors.name,
+            type: character.characterType,
+            vulnerabilities: character.colors.options,
+            svgFile: character.file,
+            faceColor: character.colors.face,
+            hairColor: character.colors.hairFront,
+            glassesId: character.colors.glasses,
+            beardsId: character.colors.beards
+        };
+        characterDbList.push(characterObject);
+    });
+    return characterDbList;
+}
 
 
 router.post('/herdimm', createCharacter);
