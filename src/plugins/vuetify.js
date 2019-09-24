85113bd7e6759cece2d78c20d6fb460204c6671e
Martin T.-B <martin_t_b@hotmail.com>
2019-09-24 12:11:42 -0400

    Not sure vuetify.js requires the import of the vue Animation

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,11 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router'
 import Home from '../components/Home'
-import Animation from '../components/Animation'
-
-
-
-
+//import Animation from '../components/Animation'
 import Vuetify from 'vuetify/lib';
 
 Vue.use(Vuetify);
