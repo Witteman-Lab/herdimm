9d89b095459fcc330e1501f8ced4624a3c24edac
Donavan <donavan.aziaka.1@ulaval.ca>
2019-10-08 18:43:09 -0400

    reajustment of carrousel and carrousel code (v-for)

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -6,147 +6,30 @@
                     <button id="btnCloseOnBoarding" class="delete" aria-label="close modal" v-on:click="this.closeOnBoarding"></button>
                 </div>
                 <v-stepper-header>
-
-                    <!-- TODO: This could probably be optimized using v-for and configuration parameters (e.g. number of steps, etc.) -->
-
-                    <v-stepper-step class="stepper-step" @click="e1 = 1" :complete="e1 > 1" step="1">{{this.labels.stepText1}}</v-stepper-step>
-                    <v-divider ></v-divider>
-
-                    <v-stepper-step class="stepper-step" @click="e1 = 2" :complete="e1 > 2" step="2">{{this.labels.stepText2}}</v-stepper-step>
-                    <v-divider></v-divider>
-
-                    <v-stepper-step class="stepper-step" @click="e1 = 3" :complete="e1 > 3" step="3">{{this.labels.stepText3}}</v-stepper-step>
-                    <v-divider></v-divider>
-
-                    <v-stepper-step class="stepper-step" @click="e1 = 4" :complete="e1 > 4" step="4">{{this.labels.stepText4}}</v-stepper-step>
-                    <v-divider></v-divider>
-
-                    <v-stepper-step class="stepper-step" @click="e1 = 5" :complete="e1 > 5" step="5">{{this.labels.stepText5}}</v-stepper-step>
-                    <v-divider></v-divider>
-
-                    <v-stepper-step class="stepper-step" @click="e1 = 6" :complete="e1 > 6" step="6">{{this.labels.stepText6}}</v-stepper-step>
-                    <v-divider></v-divider>
-
-                    <v-stepper-step class="stepper-step" @click="e1 = 7" :complete="e1 > 7" step="7">{{this.labels.stepText7}}</v-stepper-step>
-                    <v-divider></v-divider>
-
-                    <v-stepper-step class="stepper-step" @click="e1 = 8" :complete="e1 > 8" step="8">{{this.labels.stepText8}}</v-stepper-step>
-                    <v-divider></v-divider>
-
-                    <v-stepper-step class="stepper-step" @click="e1 = 9" step="9">{{this.labels.stepText9}}</v-stepper-step>
-                    </v-stepper-header>
+                    <div style="display: contents;" v-for="(step, index) in this.labels.stepsTitle">
+                        <v-stepper-step class="stepper-step" @click="e1 = index + 1" :complete="e1 > index" :step="index + 1">{{step.stepText}}</v-stepper-step>
+                        <v-divider v-if="index < 8"></v-divider>
+                        <!-- need to change value -->
+                    </div>
+                </v-stepper-header>
 
 
 
                 <v-stepper-items>
-                    <!-- TODO: This could probably be optimized using v-for and configuration parameters (e.g. number of steps, etc.) -->
-
-                    <!-- stepper-content 1 -->
-                    <v-stepper-content step="1">
-                        <v-card class="mb-12" color="#2196F3" >
-                            <p class="text-presentation" id="text-presentation_intro">{{this.labels.step1}}</p>
-                        </v-card>
-                        <div class="btn-group">
-                            <v-btn color="secondary" v-on:click="this.closeOnBoarding">{{this.labels.cancelBtn}}</v-btn>
-                            <v-btn color="primary" @click="e1 = 2">{{this.labels.continueBtn}}</v-btn>
-                        </div>
-                    </v-stepper-content>
-
-                    <!-- stepper-content 2 -->
-                    <v-stepper-content step="2">
-                        <v-card color="#2196F3">
-                            <img src="../assets/Image/Fr/image1.png" :alt="this.labels.step2">
-                            <p class="text-presentation">{{this.labels.step2}}</p>
-                        </v-card>
-                        <div class="btn-group">
-                            <v-btn color="secondary" @click="e1 = 1" >{{this.labels.backBtn}}</v-btn>
-                            <v-btn color="primary" @click="e1 = 3">{{this.labels.continueBtn}}</v-btn>
-                        </div>
-                    </v-stepper-content>
-
-                    <!-- stepper-content 3 -->
-                    <v-stepper-content step="3">
-                        <v-card color="#2196F3" >
-                            <img src="../assets/Image/Fr/image2.png" :alt="this.labels.step3">
-                            <p class="text-presentation">{{this.labels.step3}}</p>
-                        </v-card>
-                        <div class="btn-group">
-                        <v-btn color="secondary" @click="e1 = 2" >{{this.labels.backBtn}}</v-btn>
-                        <v-btn color="primary" @click="e1 = 4">{{this.labels.continueBtn}}</v-btn>
-                        </div>
-                    </v-stepper-content>
-
-                    <!-- stepper-content 4 -->
-                    <v-stepper-content step="4">
-                        <v-card color="#2196F3" >
-                            <img src="../assets/Image/Fr/image3.png" :alt="this.labels.step4">
-                            <p class="text-presentation">{{this.labels.step4}}</p>
-                        </v-card>
-                        <div class="btn-group">
-                        <v-btn color="secondary" @click="e1 = 3" >{{this.labels.backBtn}}</v-btn>
-                        <v-btn color="primary" @click="e1 = 5">{{this.labels.continueBtn}}</v-btn>
-                        </div>
-                    </v-stepper-content>
-
-                    <!-- stepper-content 5 -->
-                    <v-stepper-content step="5">
-                        <v-card color="#2196F3">
-                            <img src="../assets/Image/Fr/image4.png" :alt="this.labels.step5">
-                            <p class="text-presentation">{{this.labels.step5}}</p>
-                        </v-card>
-                        <div class="btn-group">
-                        <v-btn color="secondary" @click="e1 = 4" >{{this.labels.backBtn}}</v-btn>
-                        <v-btn color="primary" @click="e1 = 6">{{this.labels.continueBtn}}</v-btn>
-                        </div>
-                    </v-stepper-content>
-
-                    <!-- stepper-content 6 -->
-                    <v-stepper-content step="6">
-                        <v-card color="#2196F3" >
-                            <img src="../assets/Image/Fr/image5.png" :alt="this.labels.step6">
-                            <p class="text-presentation">{{this.labels.step6}}</p>
-                        </v-card>
-                        <div class="btn-group">
-                        <v-btn color="secondary" @click="e1 = 5" >{{this.labels.backBtn}}</v-btn>
-                        <v-btn color="primary" @click="e1 = 7">{{this.labels.continueBtn}}</v-btn>
-                        </div>
-                    </v-stepper-content>
-
-                    <!-- stepper-content 7 -->
-                    <v-stepper-content step="7">
-                        <v-card color="#2196F3">
-                            <img src="../assets/Image/Fr/image6.png" :alt="this.labels.step7">
-                            <p class="text-presentation">{{this.labels.step7}}</p>
-                        </v-card>
-                        <div class="btn-group">
-                        <v-btn color="secondary" @click="e1 = 6" >{{this.labels.backBtn}}</v-btn>
-                        <v-btn color="primary" @click="e1 = 8">{{this.labels.continueBtn}}</v-btn>
-                        </div>
-                    </v-stepper-content>
-
-                    <!-- stepper-content 8 -->
-                    <v-stepper-content step="8">
-                        <v-card color="#2196F3" >
-                            <img src="../assets/Image/Fr/image7.png" :alt="this.labels.step8">
-                            <p class="text-presentation">{{this.labels.step8}}</p>
-                        </v-card>
-                        <div class="btn-group">
-                        <v-btn color="secondary" @click="e1 = 7" >{{this.labels.backBtn}}</v-btn>
-                        <v-btn color="primary" @click="e1 = 9">{{this.labels.continueBtn}}</v-btn>
-                        </div>
-                    </v-stepper-content>
-
-                    <!-- stepper-content 9 -->
-                    <v-stepper-content step="9">
-                        <v-card color="#2196F3" >
-                            <img src="../assets/Image/Fr/image8.png" :alt="this.labels.step9">
-                            <p class="text-presentation">{{this.labels.step9}}</p>
-                        </v-card>
-                        <div class="btn-group">
-                        <v-btn color="secondary" @click="e1 = 8" >{{this.labels.backBtn}}</v-btn>
-                        <v-btn color="primary" v-on:click="this.closeOnBoarding" >{{this.labels.letsGetStart}}</v-btn>
-                        </div>
-                    </v-stepper-content>
+                    <div v-for="(step, index) in this.labels.stepsDescription">
+                        <v-stepper-content :step="index + 1">
+                                    <img v-if="index > 0" style="max-width: 80%" :src="require(`../assets/Image/En/${step.image}`)"
+                                         :alt="'Reload your browser'">
+                                    <v-card color="#2196F3">
+                                        <p v-if="index === 0" class="text-presentation" :style="index === 0 ? 'height: 32vh;' : ''">{{step.description}}</p>
+                                        <p v-if="index > 0" class="text-presentation" :style="index === 5 ? 'font-size: 1rem;' : ''">{{step.description}}</p>
+                                    </v-card>
+                                    <div class="btn-group" style="padding: 0">
+                                        <v-btn color="secondary" @click="changeCurrentView(index)">{{step.prevButton}}</v-btn>
+                                        <v-btn color="primary" @click="changeCurrentView(index + 2)">{{step.nextButton}}</v-btn>
+                                    </div>
+                        </v-stepper-content>
+                    </div>
                 </v-stepper-items>
             </v-stepper>
 
@@ -156,16 +39,13 @@
 
 
 <script>
-    import textsEng from "../assets/json/textsEng.json";
-    import textsFr from "../assets/json/textsFr.json";
-    import Home from "./Home";
-
     export default {
         name: "Carousel",
         data () {
             return {
                 isActive : true,
-                e1: 0
+                e1: 1,
+                nbrOfView: 9
             }
         },
 
@@ -191,7 +71,15 @@
              */
             closeOnBoarding() {
                 this.isActive = false;
-                this.e1 = 0;
+                this.e1 = 1;
+            },
+
+            changeCurrentView(index) {
+                if (index <= 0 || index >= this.labels.stepsDescription.length + 1) {
+                    this.closeOnBoarding();
+                } else {
+                    this.e1 = index;
+                }
             }
         },
         created(){},
@@ -232,12 +120,15 @@
         cursor: pointer;
     }
 
-    .text-presentation{
+    .text-presentation {
         text-align: center;
         color: white ;
         font-size: 1.2rem;
         vertical-align: middle;
         padding: 5px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
     }
 
     #text-presentation_intro {
