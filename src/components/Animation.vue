cc6b67a749a1ce3fc7dfe8b4b57ab5480fbaf455
Donavan Aziaka <donavan.aziaka@epitech.eu>
2019-11-12 20:11:42 -0500

    added a config file for production URL

--- src/components/Animation.vue
+++ src/components/Animation.vue
@@ -57,7 +57,7 @@
     import textsEng from "../assets/json/textsEng.json";
     import textsFr from "../assets/json/textsFr.json";
     import connections from "../assets/json/connections.json";
-
+    import config from "../../config/config"
 
 
     export default {
@@ -100,7 +100,7 @@
         methods: {
 
             sendCharactersToApi(chosenDB) {
-                const url =`http://localhost:3000/api/${chosenDB}/herdimm`;
+                const url =`http://${config.API_URL}/api/${chosenDB}/herdimm`;
                 let dataSent = {group: JSON.parse(localStorage.getItem("group")), totalTime: JSON.parse(localStorage.getItem("totalTime"))}
                 fetch(url, {
                     method: 'POST',
