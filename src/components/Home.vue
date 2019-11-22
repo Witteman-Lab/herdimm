924d610e2b37813367522a44de20156fdbdcda97
Nadege touko toudjeu <naddyn@MacBook-Pro-de-Nadege.local>
2019-11-22 12:34:28 -0500

    create routing between pages from Home(landing page) to Avatars and from Avatars to Animation

---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <div class="column">
+            <button id="continue" class="continue button is-primary is-success"  v-on:click="loadHome()">Next</button>
+
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home",
+        methods: {
+            loadHome(){
+                this.$router.push({name: 'Avatars',});
+            }
+        }
+    }
+
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '../components/Avartars'
+import Home from '../components/Home'
+import Avatars from '../components/Avatars'
 import Animation from '../components/Animation'
 
 Vue.use(Router);
@@ -12,6 +13,12 @@ export default new Router({
             path: '/',
             name: 'Home',
             component: Home
+        },
+        {
+            path: '/Avatars',
+            name: 'Avatars',
+            component: Avatars,
+            props: true
         },
         {
             path: '/Animation',
