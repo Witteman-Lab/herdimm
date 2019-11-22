530897882f112e81f773e963eb396c6c16f485e3
Martin T.-B <martin_t_b@hotmail.com>
2019-11-22 16:41:40 -0500

    Cleanup and change MongoDB URL to prod

--- src/components/Animation.vue
+++ src/components/Animation.vue
@@ -105,7 +105,7 @@
              */
             sendCharactersToApi(chosenDB) {
 
-                const url =`http://${config.API_URL}/api/${chosenDB}/herdimm`;
+                const url =`http://${config.API_URL_PROD}/api/${chosenDB}/herdimm`;
                 let dataSent = {
                     group: JSON.parse(localStorage.getItem("group")),
                     totalTime: JSON.parse(localStorage.getItem("totalTime")),
@@ -152,12 +152,7 @@
                     this.isAnimationStarted = true;
                     this.$refs.audioPlayer.playAudio();
                     this.isAnimationPlaying();
-                    console.log("caption checked", this.checkboxState);
-
-
                     this.sendCharactersToApi('mongodb');
-
-
                 }
             },
 
@@ -367,7 +362,6 @@
                 // why use argument instead of the parameter args ??
                 // just disable the warning but it needs to be discuss
                 let newArgs = Array.prototype.slice.call(arguments, 2);
-                console.log(args);
                 return context[functionName].apply(context, newArgs);
             },
 
@@ -993,7 +987,6 @@
         },
         created() {},
         mounted() {
-            //console.log("value of Return", this.returnUrl);
             window.scrollTo({ top: 0, behavior: 'smooth', x: 0});
             let initAnimation = false;
 
