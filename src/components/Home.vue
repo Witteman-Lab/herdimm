164774ce3cbf6ca14bfe18160d0ef1724b801fad
Martin T.-B <martin_t_b@hotmail.com>
2019-12-05 08:24:07 -0500

    Changed body background color

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,15 +9,15 @@
             <div class="column" style="padding: 20px;">
             <h1 class="title is-1 has-text-left is-spaced" style="font-weight: bolder">{{this.labels.homeTitle1}}
                 <br/>{{this.labels.homeTitle2}}<br/>{{this.labels.homeTitle3}}</h1>
-            
+
             <h6 class="subtitle is-6 has-text-left">{{this.labels.homeSubTitle1}}</h6><br/>
-            
+
             <button id="continue" class="continue button is-primary is-medium has-text-centered" style="float: left;  color: black" v-on:click="loadHome()">{{this.labels.homeBtnContinue}}
-                
+
                 <a><font-awesome-icon style="margin-left: 10px; color: black" icon="play" size="small"/></a>
-                
+
             </button>
-            
+
             <br/><br/><br/><br/>
             <button id="selectLanguage"  class="button is-primary is-rounded is-link is-small"  style="float: left;" v-on:click="this.changeLanguage">{{this.labels.language}}</button>
             </div>
@@ -59,7 +59,14 @@
 
 </script>
 
+<style>
+    body {
+        background-color: #CED9DB;
+    }
+</style>
+
 <style scoped>
+
     /*for larger and medium devices (desktop)*/
     @media screen and (min-width: 812px){
         .columns{
